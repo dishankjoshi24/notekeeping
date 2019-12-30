6904fa7e50e6d085d84a4aa5ab43220f798b8522
Dishank <djoshi@gammastack.com>
2019-12-30 23:20:12 +0530

    Define add onwer method

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,33 +3,18 @@ class NotesController < ApplicationController
   before_action :set_note, only: [:show, :edit, :update, :destroy]
   before_action :check_auth, only: [:edit, :update]
   before_action :check_reader, only: [:show]
-  # GET /notes
-  # GET /notes.json
+
   def index
     ids = Note.with_roles([:onwer, :reader, :contributor])
               .where(user: current_user).map(&:id).uniq
     @notes = Note.where(id: ids)
   end
 
-  # GET /notes/1
-  # GET /notes/1.json
   def show
-
   end
 
-  # GET /notes/new
   def new
     @note = Note.new
-  end
-
-  # GET /notes/1/edit
-  def edit
-  end
-
-  # POST /notes
-  # POST /notes.json
-  def share
-
   end
 
   def create
@@ -37,8 +22,7 @@ class NotesController < ApplicationController
     @note.user = current_user
     respond_to do |format|
       if @note.save
-        binding.pry
-        current_user.add_role :onwer, @note
+        add_onwer
         format.html { redirect_to @note, notice: 'Note was successfully created.' }
         format.json { render :show, status: :created, location: @note }
       else
@@ -48,8 +32,6 @@ class NotesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /notes/1
-  # PATCH/PUT /notes/1.json
   def update
     respond_to do |format|
       if @note.update(note_params)
@@ -62,8 +44,6 @@ class NotesController < ApplicationController
     end
   end
 
-  # DELETE /notes/1
-  # DELETE /notes/1.json
   def destroy
     @note.destroy
     respond_to do |format|
@@ -76,6 +56,10 @@ class NotesController < ApplicationController
 
   def set_note
     @note = Note.find(params[:id])
+  end
+
+  def add_onwer
+    current_user.add_role :onwer, @note
   end
 
   def check_auth
